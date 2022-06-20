6fc1d34d1308098c21a043626e252925b838f790
macenaandre <andremacenacosta@gmail.com>
2022-06-20 09:51:03 -0300

    Comparador de cartas implementado

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
-let quantidade = Number(prompt("Com quantas cartas deseja jogar: 4, 6, 8, 10, 12 ou 14?"));
 let numJogadas = 0;
 
-while (quantidade < 4 || quantidade > 14 || quantidade === 5 || quantidade === 7 ||
-     quantidade === 9 || quantidade === 11 || quantidade === 13) {
-        quantidade = Number(prompt("Com quantas cartas deseja jogar: 4, 6, 8, 10, 12 ou 14?"));
-     }   
 let nomes = ["bobross", "explody", "fiesta", "metal", "revertit", "triplets", "unicorn"];
 
-let cards1 = ["bobross", "bobross", "explody", "explody"];
+let game1 = ["bobross", "bobross", "explody", "explody"];
 
-let cards2 = ["bobross", "bobross", "explody", "explody", "fiesta", "fiesta"];
+let game2 = ["bobross", "bobross", "explody", "explody", "fiesta", "fiesta"];
 
-let cards3 = ["bobross", "bobross", "explody", "explody", "fiesta", "fiesta", "metal", "metal"];
+let game3 = ["bobross", "bobross", "explody", "explody", "fiesta", "fiesta", "metal", "metal"];
 
-let cards4 = ["bobross", "bobross", "explody", "explody", "fiesta", "fiesta", "metal", "metal", "revertit", "revertit"];
+let game4 = ["bobross", "bobross", "explody", "explody", "fiesta", "fiesta", "metal", "metal", "revertit", "revertit"];
 
-let cards5 = ["bobross", "bobross", "explody", "explody", "fiesta", "fiesta", "metal", "metal", "revertit", "revertit", 
+let game5 = ["bobross", "bobross", "explody", "explody", "fiesta", "fiesta", "metal", "metal", "revertit", "revertit", 
               "triplets", "triplets"];
 
-let cards6 = ["bobross", "bobross", "explody", "explody", "fiesta", "fiesta", "metal", "metal", "revertit", "revertit", 
+let game6 = ["bobross", "bobross", "explody", "explody", "fiesta", "fiesta", "metal", "metal", "revertit", "revertit", 
               "triplets", "triplets", "unicorn", "unicorn"];
-
 
     function comparador() { 
 	   return Math.random() - 0.5; 
@@ -30,10 +24,17 @@ let cards6 = ["bobross", "bobross", "explody", "explody", "fiesta", "fiesta", "m
         
         let elemento = document.querySelector(".cards");
 
-        if(quantidade === 4) {
+        let quantidade = prompt("Com quantas cartas deseja jogar: 4, 6, 8, 10, 12 ou 14?");
+
+        while (Number(quantidade) < 4 || Number(quantidade) > 14 || Number(quantidade) === 5 ||
+              ((Number(quantidade)%2) === 1) || isNaN(quantidade)) {
+                 quantidade = prompt("Por favor, digite um par de 4 a 14(4, 6, 8, 10, 12 ou 14)!");
+        }   
+
+        if(Number(quantidade) === 4) {
             elemento.innerHTML = "";
 
-            cards1.sort(comparador);
+            game1.sort(comparador);
 
             for(let i = 0; i < 4; i++) {
 
@@ -42,17 +43,17 @@ let cards6 = ["bobross", "bobross", "explody", "explody", "fiesta", "fiesta", "m
                <div class="back">
                  <img src="./imgsprojeto4/front.png" alt="">
                </div>
-               <div class="face escondido">
-                 <img src="./imgsprojeto4/${cards1[i]}parrot.gif" alt="">
+               <div class="front escondido">
+                 <img src="./imgsprojeto4/${game1[i]}parrot.gif" alt="">
                </div>  
                </div>
            `;
 
           }
-        } else if(quantidade === 6) {
+        } else if(Number(quantidade) === 6) {
             elemento.innerHTML = "";
 
-            cards2.sort(comparador);
+            game2.sort(comparador);
 
             for(let i = 0; i < 6; i++) {
                 elemento.innerHTML += `
@@ -60,16 +61,16 @@ let cards6 = ["bobross", "bobross", "explody", "explody", "fiesta", "fiesta", "m
                 <div class="back">
                   <img src="./imgsprojeto4/front.png" alt="">
                 </div>
-                <div class="face escondido">
-                  <img src="./imgsprojeto4/${cards2[i]}parrot.gif" alt="">
+                <div class="front escondido">
+                  <img src="./imgsprojeto4/${game2[i]}parrot.gif" alt="">
                 </div>  
                 </div>
             `;
             }
-        } else if(quantidade === 8) {
+        } else if(Number(quantidade) === 8) {
             elemento.innerHTML = "";
 
-            cards3.sort(comparador);
+            game3.sort(comparador);
 
             for(let i = 0; i < 8; i++) {
                 elemento.innerHTML += `
@@ -77,16 +78,16 @@ let cards6 = ["bobross", "bobross", "explody", "explody", "fiesta", "fiesta", "m
                 <div class="back">
                   <img src="./imgsprojeto4/front.png" alt="">
                 </div>
-                <div class="face escondido">
-                  <img src="./imgsprojeto4/${cards3[i]}parrot.gif" alt="">
+                <div class="front escondido">
+                  <img src="./imgsprojeto4/${game3[i]}parrot.gif" alt="">
                 </div>  
                 </div>
             `;
             }
-        } else if(quantidade === 10) {
+        } else if(Number(quantidade) === 10) {
             elemento.innerHTML = "";
 
-            cards4.sort(comparador);
+            game4.sort(comparador);
 
             for(let i = 0; i < 10; i++) {
                 elemento.innerHTML += `
@@ -94,16 +95,16 @@ let cards6 = ["bobross", "bobross", "explody", "explody", "fiesta", "fiesta", "m
                 <div class="back">
                   <img src="./imgsprojeto4/front.png" alt="">
                 </div>
-                <div class="face escondido">
-                  <img src="./imgsprojeto4/${cards4[i]}parrot.gif" alt="">
+                <div class="front escondido">
+                  <img src="./imgsprojeto4/${game4[i]}parrot.gif" alt="">
                 </div>  
                 </div>
             `;
             }
-        } else if(quantidade === 12) {
+        } else if(Number(quantidade) === 12) {
             elemento.innerHTML = "";
 
-            cards5.sort(comparador);
+            game5.sort(comparador);
 
             for(let i = 0; i < 12; i++) {
                 elemento.innerHTML += `
@@ -111,16 +112,16 @@ let cards6 = ["bobross", "bobross", "explody", "explody", "fiesta", "fiesta", "m
                 <div class="back">
                   <img src="./imgsprojeto4/front.png" alt="">
                 </div>
-                <div class="face escondido">
-                  <img src="./imgsprojeto4/${cards5[i]}parrot.gif" alt="">
+                <div class="front escondido">
+                  <img src="./imgsprojeto4/${game5[i]}parrot.gif" alt="">
                 </div>  
                 </div>
             `;
             }
-        } else if(quantidade === 14) {
+        } else if(Number(quantidade) === 14) {
             elemento.innerHTML = "";
 
-            cards6.sort(comparador);
+            game6.sort(comparador);
 
             for(let i = 0; i < 14; i++) {
                 elemento.innerHTML += `
@@ -128,8 +129,8 @@ let cards6 = ["bobross", "bobross", "explody", "explody", "fiesta", "fiesta", "m
                 <div class="back">
                   <img src="./imgsprojeto4/front.png" alt="">
                 </div>
-                <div class="face escondido">
-                  <img src="./imgsprojeto4/${cards6[i]}parrot.gif" alt="">
+                <div class="front escondido">
+                  <img src="./imgsprojeto4/${game6[i]}parrot.gif" alt="">
                 </div>  
                 </div>
             `;
@@ -139,6 +140,32 @@ let cards6 = ["bobross", "bobross", "explody", "explody", "fiesta", "fiesta", "m
     distribuirCartas();
 
     function selecionarCarta(elemento) {
+
+
+        if(elemento.querySelector(".front").classList.contains("escondido")) {
+            numJogadas ++;
+         }
+
         elemento.querySelector(".back").classList.add("escondido");
-        elemento.querySelector(".face").classList.remove("escondido");
+        elemento.querySelector(".front").classList.remove("escondido");
+
+        function desviraCarta() {
+            elemento.querySelector(".back").classList.remove("escondido");
+            elemento.querySelector(".front").classList.add("escondido");
+        }
+
+        if((numJogadas % 2 !== 0)) {
+            elemento.classList.add("clicked1");   
+        }
+
+        if((numJogadas % 2 === 0)) {
+            elemento.classList.add("clicked2");
+         
+            if(document.querySelector(".clicked1 .front").innerHTML === document.querySelector(".clicked2 .front").innerHTML) {
+                document.querySelector(".clicked1").classList.remove("clicked1");
+                document.querySelector(".clicked2").classList.remove("clicked2");
+            }
+        }
+
     }
+    
